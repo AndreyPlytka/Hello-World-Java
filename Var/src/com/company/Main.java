a020c2608c302c0210dcb17264e58d33d86a79e2
AndriyPlytka <andriyplutka@gmail.com>
2020-07-09 01:52:11 +0300

    Java Lesson 1 Hello World

---
+++ Var/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.lang.String;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    byte num = 23;
+	    int num_1 = 34321;
+
+	    float num_2 = 23.45f;
+	    double num_3 = 213123.3242341d;
+
+	    boolean isTrue = true;
+	    boolean isFalse = false;
+	    char sym = 'R';
+
+	    String str = "Hello World!";
+
+
+	    System.out.print(str);
+
+    }
+}
